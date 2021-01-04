630abfaa5642d59082d8a35a23a10dbba7b2f7b9
DireStraits01 <arsamak88@gmail.com>
2021-01-04 21:37:21 +0300

    create login page

--- prof/urls.py
+++ prof/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.yo_profile, name='yo_profile'),
-    path('login_page/', views.login_page, name='login_page'),
-    path('registration_page/', views.registration_page,
-         name='registration_page'),
+    path('login/', views.login, name='login'),
+
 ]
--- prof/views.py
+++ prof/views.py
@@ -5,9 +5,5 @@ def yo_profile(request):
     return render(request, 'prof/yo_profile.html')
 
 
-def login_page(request):
-    return render(request, 'prof/login_page.html')
-
-
-def registration_page(request):
-    return render(request, 'prof/registration_page.html')
+def login(request):
+    return render(request, 'prof/login.html')
