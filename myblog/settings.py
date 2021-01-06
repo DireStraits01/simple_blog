195d4e1eba9d63d88c55b3460a675411196fbfc3
DireStraits01 <arsamak88@gmail.com>
2021-01-06 13:10:13 +0300

    created css style for  home page when target to images , they are  zoom

--- myblog/settings.py
+++ myblog/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = '3eqq@i+3cmlz67!g7!lhg=mr+7_=ctw&bbufw%(g^#l9lgg=yd'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+# STATICFILES_DIRS = [
+#   "/home/lors/Desktop/Projects/myblog/static"
+# ]
 
 # Application definition
 
@@ -127,7 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
