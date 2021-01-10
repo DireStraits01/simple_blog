be293787953a9cd27e540965bd97f4867c1057a2
DireStraits01 <arsamak88@gmail.com>
2021-01-10 08:11:14 +0300

    add function register user from html page and create register_done page

--- prof/urls.py
+++ prof/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.yo_profile, name='yo_profile'),
     path('login/', views.login, name='login'),
-    path('registration/', views.registration, name='registration'),
+    path('registration/', views.registr, name='registration'),
+    path('register_done/', views.registr, name='register_done')
 
 ]
--- prof/views.py
+++ prof/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 
 
 def yo_profile(request):
@@ -9,5 +11,15 @@ def login(request):
     return render(request, 'prof/login.html')
 
 
-def registration(request):
-    return render(request, 'prof/registration.html')
+def registr(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.save()
+
+            return render(request, 'prof/register_done.html')
+    else:
+        user_form = UserCreationForm()
+    context = {'user_form': user_form}
+    return render(request, 'prof/registration.html', context)
