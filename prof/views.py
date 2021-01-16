05034fdea00255e1c441e8efca60ae2804114333
DireStraits01 <arsamak88@gmail.com>
2021-01-16 04:04:47 +0300

    add style for send buttons on the articals and comment pages

--- prof/views.py
+++ prof/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import auth, User
 from django.contrib import messages
